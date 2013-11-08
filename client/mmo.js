a635daea3a4be6993488169001391044b3ce23f9
bobbybee <alyssa@Students-MacBook-Air.local>
2013-11-08 15:56:31 -0500

    Began skeleton for WebSocket client

---
+++ client/mmo.js
@@ -0,0 +1,40 @@
+/*
+mmo.js
+mmo.js
+WebSocket client
+Copyright (C) 2013 bobbybee
+ALL RIGHTS RESERVED
+*/
+
+function MMO(host, port){
+    this.host = host ? host : "localhost";
+    this.port = port ? port : 1337;
+
+    this.isConnected = false;
+    this.sock = null;
+}
+
+MMO.prototype.connect = function(){
+    this.sock = new WebSocket("ws://"+this.hort+":"+this.port);
+        
+    this.sock.onopen = this.onopen;
+    this.sock.onclose = this.onclose;
+    this.sock.onmessage = this.onmessage;
+    this.sock.onerror = this.onerror;
+};
+
+MMO.prototype.onopen = function(e){
+    
+};
+
+MMO.prototype.onclose = function(e){
+    
+};
+
+MMO.prototype.onmessage = function(e){
+    var d = e.data;
+};
+
+MMO.prototype.onerror = function(e){
+    
+};
